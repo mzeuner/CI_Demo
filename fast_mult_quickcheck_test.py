da155d86873c44454f5fc4789b1bfc7a2dfd2910
mzeuner <max@zeuner.net>
2023-09-12 10:58:42 +0200

    drop quickcheck

--- fast_mult_quickcheck_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from pytest import list_of
-from fast_mult import mul_poly, karatsuba
-
-@pytest.mark.randomize(p=list_of(int), q=list_of(int), ncalls=100)
-def test_randomized(p,q):
-    assert(mul_poly(p,q) == karatsuba(p,q))
